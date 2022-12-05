4e6446b40f7fc71edfc4733c092767c7370fa7ca
Jesse Taube <Mr.Bossman075@gmail.com>
2022-12-04 21:33:51 -0500

    Fix warnings for eth_hw_addr_set and event->bssid
    esp_set_mac_address wont change the mac address of the device, so
    we can remove it acording to the kernel documentation.
    
    !event->bssid will always be true assuming that event is not NULL,
    so we can remove the check.
    
    Use eth_hw_addr_set instead of ether_addr_copy to set the mac address
    of the device for >= 5.15.
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=f3956ebb3bf06ab2266ad5ee2214aed46405810c
    
    request->ssids can be NULL, so we need to check it before using it.
    
    Signed-off-by: Jesse Taube <Mr.Bossman075@gmail.com>

--- esp_hosted_ng/host/esp_cmd.c
+++ esp_hosted_ng/host/esp_cmd.c
@@ -538,10 +538,6 @@ static void process_connect_status_event(struct esp_wifi_device *priv,
 
 	printk(KERN_INFO "Connection status: %d\n", event->header.status);
 
-	if (!event->bssid) {
-		printk(KERN_INFO "bssid input as NULL. Ignoring the connect statuc event\n");
-		return;
-	}
 	memcpy(mac, event->bssid, MAC_ADDR_LEN);
 
 	cfg80211_connect_result(priv->ndev, mac, NULL, 0, NULL, 0,
@@ -1064,7 +1060,7 @@ int cmd_scan_request(struct esp_wifi_device *priv, struct cfg80211_scan_request 
 			sizeof(struct esp_payload_header));
 
 	/* TODO: Handle case of multiple SSIDs or channels */
-	if(request->ssids[0].ssid_len) {
+	if(request->ssids && request->ssids[0].ssid_len) {
 		memcpy(scan_req->ssid, request->ssids[0].ssid, MAX_SSID_LEN);
 	}
 
